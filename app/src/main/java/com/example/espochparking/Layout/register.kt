921c0e6ef161df7f8703ef380b29c9eb92c0fb61
JhoannBustamante <JhoannBustamanteFP@gmail.com>
2025-06-16 17:08:54 -0500

    login,register,....

---
+++ app/src/main/java/com/example/espochparking/Layout/register.kt
@@ -0,0 +1,2 @@
+package com.example.espochparking.Layout
+
--- app/src/main/java/com/example/espochparking/MainActivity.kt
+++ app/src/main/java/com/example/espochparking/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.espochparking
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-
         }
     }
 }
+
+
+
+
+
